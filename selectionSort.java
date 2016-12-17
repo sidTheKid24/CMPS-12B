b6cc3dfd459cc33e9b60fbb8ede99d48a9af86d3
Siddharth <siddharthb2481@gmail.com>
2016-12-16 19:52:21 -0800

    Added comments to the file selectionSort.java

--- selectionSort.java
+++ selectionSort.java
@@ -1,9 +1,14 @@
 public class selectionSort {
+	// This is the main method
 	public static void main(String[] args){
+		// this is an unsorted array
 		int[] array = {6,3,8,4,9,7,1,2,5};
+		// runs selection sort on unsorted array
 		selectionSort(array);
+		// print out the array
 		printArray(array);
 	}
+	// this method is for selection sort
 	public static void selectionSort (int[] array){
 		int i, j, temp;  
 		for (i = 0; i < array.length - 1; i ++){
@@ -22,6 +27,7 @@ public class selectionSort {
 			}
 		}           
 	}
+	// this method is for bubble sort
 	public static void bubbleSort(int[] array){
 		int bottom = array.length - 1;
 		for(int top = 0; top < bottom; top++){
@@ -34,6 +40,7 @@ public class selectionSort {
 			}
 		}
 	}
+	// this method prints out the array passed to it
 	public static void printArray(int[] array){
 		for(int i = 0; i < array.length; i ++){
 			System.out.print(array[i] + " ");
